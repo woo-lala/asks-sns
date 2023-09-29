3b690521d3f7319afad2ef754e7c333d4e1579f9
woo-lala <hyunj2034@naver.com>
2023-09-29 16:43:26 +0900

    init : Initialize project

---
+++ src/main/java/com/sns/asks/AsksApplication.java
@@ -0,0 +1,13 @@
+package com.sns.asks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sns/asks/AsksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sns.asks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
